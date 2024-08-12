4e2b986fb15f166b5e765a8ed608010dd1593c3d
Mohamed Amine Guizani <amineguizani33@gmail.com>
2024-08-12 20:13:23 +0100

    worked on JWT auth and fixed some parts in the frontend

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import { useSignup } from "../../hooks/useSignup";
 
  
 
@@ -8,16 +8,20 @@ function SignUp() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const {signup , error, isLoading} = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await axios.post('/api/signup', { username, email, password });
-      console.log(response.data);
-    } catch (err) {
-      setError(err.response.data.error);
-    }
+    await signup(username,email,password)
+  };
+  const handleUsername = (event)=> {
+    setUsername(event.target.value)
+  }
+  const handleEmail = (event) => {
+    setEmail(event.target.value)
+  };
+  const handlePassword = (event)=>{
+    setPassword(event.target.value)
   };
 
   return (
@@ -28,30 +32,36 @@ function SignUp() {
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-2xl text-white font-bold">Sign-Up</h1>
         <input 
+        onChange={handleUsername}
           type="text" 
           placeholder="Username" 
           className="p-2 rounded w-full bg-gray-800 text-white border border-gray-700"
         />
         <input 
+          onChange={handleEmail}
           type="email" 
           placeholder="Email" 
           className="p-2 rounded w-full bg-gray-800 text-white border border-gray-700"
         />
         <input 
+        onChange={handlePassword}
           type="password" 
           placeholder="Password" 
           className="p-2 rounded w-full bg-gray-800 text-white border border-gray-700"
         />
         <button 
-          type="submit" 
+          
+          type="submit"
+          onClick={handleSubmit} 
           className="bg-orange-600 text-white p-2 rounded w-full mt-4 hover:bg-orange-500"
         >
-          Login
+          Signup
         </button>
+        {error && <div className="">{error}</div>}
         
       </div>
       <div className="bg-gray-800 flex flex-col items-center w-full rounded-b-2xl shadow-b-2xl p-6">
-        <p className="text-white text-center">you have an acount</p>
+        <p className="text-white text-center">You Have An Acount ? </p>
         <Link href="/login" className="text-white bg-orange-600 text-center w-[50%] rounded-lg p-2 mt-2 hover:bg-orange-500">Log In</Link>
       </div>
     </form>

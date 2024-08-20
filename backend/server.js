8f3f094672e8261e3baf47f9946eb46a05e25002
khlifi ayhem <khlifiayhem6@gmail.com>
2024-08-20 13:04:46 +0100

    vercel cofig

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,29 @@ const dotenv = require('dotenv')
 const movieroutes=require('./Routes/movies')
 const userroutes= require('./Routes/users')
 const reviewRoutes = require('./Routes/reviews')
+const cors = require('cors')
 
 dotenv.config()
 const app = express()
 
-
+const port = process.env.PORT || 4000
 
 //middleware to analyse the on comming http request as json format
 app.use(express.json())
-
+app.use(cors())
 
 
 mongoose.connect(process.env.mongodburl)
 .then(()=>{
     console.log("connected to the database");
-    app.listen(process.env.PORT,()=>{
+    app.listen(port,()=>{
         console.log("app listening on port ")
     })
 })
-
+app.use('/',(req,res)=>{res.send("hello")})
 app.use('/movies',movieroutes)
 app.use('/users',userroutes)
 app.use('/reviews', reviewRoutes);
 
+
+module.exports = app ;

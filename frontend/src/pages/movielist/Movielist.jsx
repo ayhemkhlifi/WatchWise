bac861c367b78ef7addff672c11e72547e1c1e5b
Mohamed Amine Guizani <amineguizani33@gmail.com>
2024-08-26 00:14:07 +0100

    added api url to everything

--- frontend/src/pages/movielist/Movielist.jsx
+++ frontend/src/pages/movielist/Movielist.jsx
@@ -22,7 +22,7 @@ function Movielist() {
         
        
       }
-       const response = await fetch('/movies',getoptions)
+       const response = await fetch('https://watchwise-3.onrender.com/movies',getoptions)
        const json = await response.json()
        setMovies(json)
        setLoading(false)

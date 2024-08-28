bca35148800818a2855528ff4e0e11a7b3fa44d2
ayhemkhlifi <151956843+ayhemkhlifi@users.noreply.github.com>
2024-08-28 21:09:41 +0100

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -9,7 +9,7 @@ export const useSignup = ()=>{
     const signup = async (username , email , password)=>{
         setIsLoading(true)
         setError(null)
-        const response = await fetch('https://watchwise-3.onrender.com/users/signup',{
+        const response = await fetch('https://watch-wise-yish.vercel.app/users/signup',{
             method:'POST',
             headers:{'Content-Type':'application/json'},
             body: JSON.stringify({username,email,password})

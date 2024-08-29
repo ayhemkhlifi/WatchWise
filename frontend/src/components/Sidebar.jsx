c8c44dcd8b782d168ab4987163fbe82dc45a2215
Mohamed Amine Guizani <amineguizani33@gmail.com>
2024-08-29 13:15:28 +0100

    fixed sign up  / sign in page and the side bar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -23,24 +23,24 @@ function Sidebar({ isOpen, onClose }) {
       >
         <X />
       </button>
-      <img src="/images/WatchWise_prev_ui.png" alt="logo" className="h-16 w-auto" />
+      <img src="/images/WatchWise_prev_ui.png" alt="logo" className="relative h-16 w-auto" />
 
       
         <ul className="flex flex-col items-center py-10 gap-10">
           <Link to="/" onClick={onClose} className="text-white text-lg">Home</Link>
           <Link to="/choose-filter" onClick={onClose} className="text-white text-lg">Discover</Link>
-         { !user&&<Link to="/login" onClick={onClose} className=" flex gap-2 hover:bg-orange-500 p-2 rounded-xl">
+        
+          {user && <Link to='/movielist' onClick={onClose} className="hover:bg-orange-500 hover:text-black p-2 rounded-xl" >WatchedList</Link> } 
+          { !user&&<Link to="/login" onClick={onClose} className=" absolute bottom-20 flex gap-2 hover:bg-orange-500 p-2 rounded-xl">
             <User />
             <h1 className="text-white text-lg">Log-In</h1>
           </Link>}
           { user&&
-            <Link to="/" onClick= {logout} className=" flex gap-2 hover:bg-orange-500 p-2 rounded-xl">
+            <Link to="/" onClick= {logout} className=" absolute bottom-20 flex gap-2 hover:bg-orange-500 p-2 rounded-xl">
             <User />
             <h1 className="text-white text-lg">Log-out</h1>
           </Link>
           }
-          {user && <Link to='/movielist' onClick={onClose} className="hover:bg-orange-500 hover:text-black p-2 rounded-xl" >WatchedList</Link> } 
-
           
         </ul>
       
